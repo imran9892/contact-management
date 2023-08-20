45d6e07cdd99f38da1a4b1e228eb4fe3e9de9dfc
Imran <ioimranothman@gmail.com>
2023-08-21 00:05:09 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Outlet } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex gap-1 py-3 px-1 md:gap-2 max-w-7xl mx-auto min-h-screen">
+      {/* Side Bar (Menu) */}
+      <div className="flex-none w-1/5 bg-slate-200 max-h-screen rounded-xl">
+        <div className="px-0 py-3 gap-6 mx-1 flex flex-col text-slate-900 md:text-xl sm:p-4 lg:p-8 md:gap-12">
+          <h2 className="font-bold text-slate-950 uppercase">Menu</h2>
+          <nav className="flex flex-col gap-4 lg:gap-8">
+            <NavLink
+              to={'/contacts'}
+              className={({ isActive }) =>
+                isActive
+                  ? 'text-sky-600 font-bold mr-auto'
+                  : 'hover:text-slate-500 mr-auto'
+              }
+            >
+              Contacts
+            </NavLink>
+            <NavLink
+              to={'/charts-maps'}
+              className={({ isActive }) =>
+                isActive
+                  ? 'text-sky-600 font-bold mr-auto'
+                  : 'hover:text-slate-500 mr-auto'
+              }
+            >
+              Charts and Maps
+            </NavLink>
+          </nav>
+        </div>
+      </div>
+      {/* Main Content (Contacts and Chart-Map Side) */}
+      <div className="flex-initial w-4/5 bg-orange-200 rounded-xl">
+        <main className="p-2 mx-2 text-slate-50 md:text-xl md:p-8 md:gap-14">
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 }
